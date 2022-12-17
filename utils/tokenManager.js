e3efcbc72211a2395dba99f4ac21f9e9eca4907c
Kilimanjjjaro <hola@kilimanjjjaro.com>
2022-12-17 14:15:57 -0300

    token payload updated

--- utils/tokenManager.js
+++ utils/tokenManager.js
@@ -5,13 +5,9 @@ export const tokenGenerator = (uid, role) => {
   const expiresIn = 60;
 
   try {
-    const accessToken = jwt.sign(
-      { uid, email, username, role },
-      process.env.ACCESS_KEY,
-      {
-        expiresIn,
-      }
-    );
+    const accessToken = jwt.sign({ uid, role }, process.env.ACCESS_KEY, {
+      expiresIn,
+    });
     return { accessToken, expiresIn };
   } catch (error) {
     console.error(error);
@@ -23,13 +19,9 @@ export const refreshTokenGenerator = (uid, role, res) => {
   const expiresIn = 120;
 
   try {
-    const refreshToken = jwt.sign(
-      { uid, email, username, role },
-      process.env.REFRESH_KEY,
-      {
-        expiresIn,
-      }
-    );
+    const refreshToken = jwt.sign({ uid, role }, process.env.REFRESH_KEY, {
+      expiresIn,
+    });
 
     res.cookie("refreshToken", refreshToken, {
       // httpOnly: true,
