9fa5f30e0e676ed1bdc90d2524834d07c640efb2
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-03-14 21:58:01 -0300

    Clear console log

--- middlewares/requireAccessToken.middleware.js
+++ middlewares/requireAccessToken.middleware.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 export const requireAccessToken = (req, res, next) => {
-  console.log('cookies:', req.headers)
   try {
     let accessToken = req.headers?.authorization;
 
