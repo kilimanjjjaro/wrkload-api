5aa0155daa028d003854e12f11064e5d33011b30
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-08-10 20:02:04 -0300

    Test

--- utils/transporter.js
+++ utils/transporter.js
@@ -7,8 +7,7 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  secure: true
+  }
 });
 
 transporter.verify().then(() => {
