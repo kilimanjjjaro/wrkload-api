b293c6bafbd8ad46c700ffffc288137c060f228d
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-08-10 20:07:03 -0300

    Test

--- utils/transporter.js
+++ utils/transporter.js
@@ -1,13 +1,16 @@
 import "dotenv/config";
 import nodemailer from "nodemailer";
 
+console.log(process.env.EMAIL_USER + "    " + process.env.EMAIL_PASS);
+
 const transporter = nodemailer.createTransport({
   host: "c2390804.ferozo.com",
   port: 465,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  }
+  },
+  secure: true
 });
 
 transporter.verify().then(() => {
